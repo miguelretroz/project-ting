ffe457fe68e6d98f1e76bdc0c225b7caa3bdfe96
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-06 15:25:18 -0300

    Requirement 02-> Implement function `txt_importer`

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, "r") as file:
+            content = file.read()
+            lines = content.split("\n")
+
+            return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
