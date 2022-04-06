2f28bd66ba8b5230c02e0060c4b4cd57ba725cf0
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-06 16:19:27 -0300

    Requirement 07-> Implement function `search_by_word`

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,7 +1,16 @@
 import re
 
 
-def exists_word(word, instance):
+def format_occurrence(word, path_file, occurrences, list):
+    if len(occurrences):
+        list.append({
+            "palavra": word,
+            "arquivo": path_file,
+            "ocorrencias": occurrences,
+        })
+
+
+def exists_word(word, instance, insert_content=False):
     result = []
 
     for index in range(len(instance)):
@@ -11,17 +20,19 @@ def exists_word(word, instance):
 
         for line_index, line in enumerate(lines):
             if re.search(rf"(?<!\w){word}(?!\w)", line, flags=re.IGNORECASE):
-                occurrences.append({"linha": (line_index + 1)})
+                occurrence_content = {
+                    "linha": (line_index + 1)
+                }
 
-        if len(occurrences):
-            result.append({
-                "palavra": word,
-                "arquivo": file_data["nome_do_arquivo"],
-                "ocorrencias": occurrences,
-            })
+                if insert_content:
+                    occurrence_content["conteudo"] = line
+                occurrences.append(occurrence_content)
+
+        format_occurrence(
+            word, file_data["nome_do_arquivo"], occurrences, result)
 
     return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return exists_word(word, instance, insert_content=True)
