f38c04d38f1dd50a62003c705d4edea857725468
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-06 15:10:24 -0300

    Requirement 01-> Implement method of `Queue` class

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,24 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not len(self):
+            return None
+
+        return self._data.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index <= (len(self) - 1):
+            return self._data[index]
+        else:
+            raise IndexError
