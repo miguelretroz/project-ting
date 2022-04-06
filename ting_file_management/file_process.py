da1980004721fd07a16fe8852bc1925695c78752
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-06 15:37:34 -0300

    Requirement 03-> Implement function `process`

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        curr_file = instance.search(index)
+        if curr_file["nome_do_arquivo"] == path_file:
+            return None
+
+    lines = txt_importer(path_file)
+    processed_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(processed_data)
+    print(processed_data)
+
+    return processed_data
 
 
 def remove(instance):
